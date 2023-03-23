8a97ec21be08d77d5dd0a1cd48e93720c7680f3b
C1330938 AronRibeiro <aronr.chaves@ibm.com>
2023-03-23 19:58:39 -0300

    start

---
+++ src/pages/pricing.js
@@ -0,0 +1,14 @@
+import { Button, Card, Header, Navbar } from '../components';
+import React from 'react';
+
+function payment() {
+	return (
+		<div>
+			<Navbar />
+			<Header />
+			<Card />
+		</div>
+	);
+}
+
+export default payment;
