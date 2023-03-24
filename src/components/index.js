8e1fd02157dbbf23462174500b8de56f3b9d1a31
C1330938 AronRibeiro <aronr.chaves@ibm.com>
2023-03-24 04:12:49 -0300

    removing imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-export { default as Button } from './Button.jsx';
-
 export { default as Navbar } from './Navbar.jsx';
 
 export { default as Card } from './Card.jsx';
 
 export { default as Header } from './Header.jsx';
+
+export { default as Pricing } from './Header.jsx';
