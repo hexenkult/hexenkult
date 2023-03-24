80b0e6e40b590039b41409094d621936301e2c34
unlessWeFall <34076730+unlessifail@users.noreply.github.com>
2023-03-23 21:24:35 -0300

    commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import styles from '../styles/Card.module.css';
 
 function Card() {
-	return <div>Card</div>;
+	return<div className={styles.container}>
+		<div className={styles.content}>Card</div>
+	</div>;
+		
 }
 
 export default Card;
