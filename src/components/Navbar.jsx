68511355c9f8ea342c7099711a793e9015ea86f2
C1330938 AronRibeiro <aronr.chaves@ibm.com>
2023-03-24 04:13:00 -0300

    new navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,49 @@
-import React from 'react'
-import styles from '../styles/Navbar.module.css'
+import React from 'react';
+import styles from '../styles/Navbar.module.css';
+import { Button, Text, Navbar, Link } from '@nextui-org/react';
 
-function Navbar() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.logo}>
-        <a href="">
-          <img src="/logo.png" alt="." />
-        </a>
-      </div>
-      <div className={styles.lista}>
-        <ul className={styles.navbar}>
-          <li>Home</li>
-          <li>Princing</li>
-          <li>Blog</li>
-          <li>About Us</li>
-        </ul>
-      </div>
+function mNavbar() {
+	return (
+		<div className={styles.parent}>
+			<Navbar isBordered className={styles.container} variant={'sticky'}>
+				<Navbar.Brand>
+					<img src="/logo.png" alt="hexenkult logo" />
+				</Navbar.Brand>
+				<Navbar.Content hideIn="xs">
+					<Navbar.Link href="#">Home</Navbar.Link>
+					<Navbar.Link isActive activeColor={'secondary'} href="#">
+						Pricing
+					</Navbar.Link>
+					<Navbar.Link href="#">Pricing</Navbar.Link>
+					<Navbar.Link href="#">Blog</Navbar.Link>
+					<Navbar.Link href="#">About Us</Navbar.Link>
+				</Navbar.Content>
+				<Navbar.Content>
+					<Navbar.Link color="inherit" href="#">
+						login
+					</Navbar.Link>
 
-      <div>
-        <p className={styles.login}>Login</p>
-      </div>
-    </div>
-  );
+					<Navbar.Item>
+						<Button
+							auto
+							flat
+							as={Link}
+							href="#"
+							css={{
+								background: 'white',
+								color: 'black',
+								width: '158px',
+								height: '45px',
+								border: '4px solid white',
+							}}
+						>
+							Get Free Access
+						</Button>
+					</Navbar.Item>
+				</Navbar.Content>
+			</Navbar>
+		</div>
+	);
 }
 
-export default Navbar
+export default mNavbar;
