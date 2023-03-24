ca6ca252cd25745d5513ded1f99e6d6f88d1e1bf
C1330938 AronRibeiro <aronr.chaves@ibm.com>
2023-03-23 22:14:11 -0300

    mudando logo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,14 @@
 import styles from '../styles/Button.module.css';
 import React from 'react';
 import { Button } from '@nextui-org/react';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
+
+const theme = createTheme({});
 
 function mButton() {
 	return (
 		<div>
-			<Button>Get Started</Button>
+			<Button theme={theme.colors.primary}>Get Started</Button>
 		</div>
 	);
 }
