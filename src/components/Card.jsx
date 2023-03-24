7ebe034c6eebde9efea92ee35f2a4a57a9fa0dab
C1330938 AronRibeiro <aronr.chaves@ibm.com>
2023-03-23 23:28:25 -0300

    creating and styling card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
 import styles from '../styles/Card.module.css';
+import { Card, Grid, Text, Row } from '@nextui-org/react';
+import Button from './Button';
 
-function Card() {
-	return<div className={styles.container}>
-		<div className={styles.content}>Card</div>
-	</div>;
-		
+function mCard(props) {
+	const list = [
+		{
+			title: 'Orange',
+			img: '/images/fruit-1.jpeg',
+			price: '$5.50',
+		},
+		{
+			title: 'Tangerine',
+			img: '/images/fruit-2.jpeg',
+			price: '$3.00',
+		},
+		{
+			title: 'Cherry',
+			img: '/images/fruit-3.jpeg',
+			price: '$10.00',
+		},
+	];
+
+	return (
+		<>
+			<Grid.Container gap={2}>
+				<Grid sm={12} md={5}>
+					<Card css={{ mw: '330px' }}>
+						<Card.Header>
+							<Text b>{props.planName}</Text>
+						</Card.Header>
+						<Card.Body css={{ py: '$10' }}>
+							<Text justify="">
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</Text>
+						</Card.Body>
+
+						<Card.Footer>
+							<Row justify="center">
+								<Button btnTitle="Get Started" />
+							</Row>
+						</Card.Footer>
+					</Card>
+				</Grid>
+			</Grid.Container>
+		</>
+	);
 }
 
-export default Card;
+export default mCard;
